ed881d2f70eabac99b3b9d0aae72262146691ce0
alastopa <71985076+alastopa@users.noreply.github.com>
2020-09-27 21:18:15 +0200

    Create program_1.py

---
+++ program_1.py
@@ -0,0 +1,16 @@
+print("------ Dzialanie 1 ------")
+b = float(input("Podaj liczbę b: "))
+a = b ** 3 + 5
+print("a = ", a)
+print()
+
+print("------ Dzialanie 2 ------")
+b = float(input("Podaj liczbę b: "))
+a = (4 - b) / 5
+print("a = ", a)
+print()
+
+print("------ Dzialanie 3 ------")
+b = float(input("Podaj liczbę b: "))
+a = (2 ** 3 + b) / 3 ** 2
+print("a = ", a)
